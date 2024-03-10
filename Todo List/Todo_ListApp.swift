c6768169c357e67231584def58ca65ac7f346692
a.krasnov <wonsarc@icloud.com>
2024-03-10 17:28:06 +0300

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Artem Krasnov on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
